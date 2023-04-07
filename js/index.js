8fdd814825f76a92e62bdc5c8ed5e4588c56118d
jrag-dev <josealvarado2117@gmail.com>
2023-04-07 04:14:36 -0400

    Desktop created successfully

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 import validarForm from './form.js';
-import focusLink from './links.js';
+import { focusLink, focusLinkSocial } from './links.js';
 
 const d = document;
 
 d.addEventListener('DOMContentLoaded', (e) => {
 	console.log('DOMContentLoaded');
 	validarForm('.contact__form', '.contact__input', '.contact__btn', '.contact__field');
-	focusLink('.team__cta', '.team__cta-img-green', '.team__cta-img-white')
+	focusLink('.team__cta', '.team__cta-img-green', '.team__cta-img-white');
+	focusLinkSocial('.footer__social', '.socials__img-normal', '.socials__img-hover');
 })
--- js/links.js
+++ js/links.js
@@ -1,7 +1,7 @@
 const d = document;
 
 
-export default function focusLink(link, img1, img2) {
+export function focusLink(link, img1, img2) {
 	const $link = d.querySelector(link);
 
 	d.addEventListener('mouseover', (e) => {
@@ -13,5 +13,31 @@ export default function focusLink(link, img1, img2) {
 			d.querySelector(img2).classList.remove('active')
 		}
 		e.preventDefault();
+	})
+}
+
+
+export const focusLinkSocial = (link, img1, img2) => {
+	const $img = d.querySelectorAll(img1);
+	const $imgHover = d.querySelectorAll(img2);
+
+	d.addEventListener('mouseover', (e) => {
+		if (e.target.matches(link) || e.target.matches(`${link} *`)) {
+			if (e.target.classList.contains(link.slice(1,))) {
+				e.target.classList.add('footer__social-active')
+			} else {
+				e.target.parentElement.classList.add('footer__social-active')
+			}
+		}
+	})
+
+	d.addEventListener('mouseout', (e) => {
+		if (e.target.matches(link) || e.target.matches(`${link} *`)) {
+			if (e.target.classList.contains(link.slice(1,))) {
+				e.target.classList.remove('footer__social-active')
+			} else {
+				e.target.parentElement.classList.remove('footer__social-active')
+			}
+		}
 	})
 }
